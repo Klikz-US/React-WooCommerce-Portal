c558455576b4d389d247867782e4290347f79eec
Murrell <dev@klikz.us>
2020-12-08 11:45:54 -0500

    dev

--- server/services/mongoose.js
+++ server/services/mongoose.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const db_addr = process.env.DB_ADDR_PRODUCTION;
-// const db_addr = process.env.DB_ADDR;
+// const db_addr = process.env.DB_ADDR_PRODUCTION;
+const db_addr = process.env.DB_ADDR;
 let count = 0;
 
 const options = {
