c7d6cf9b717c933e0df749313970251f00fb60a4
Murrell Johnson <dev@klikz.us>
2020-12-20 16:30:08 -0500

    merge servers

--- staging/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import auth from './auth.reducer';
-
-// to combine all reducers together
-const appReducer = combineReducers({
-    auth
-});
-
-export default appReducer;
